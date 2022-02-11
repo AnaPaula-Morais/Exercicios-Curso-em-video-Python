e362fba48de4989804b7bfa280507d5e98600607
Ana Paula <ap.almeidapm@gmail.com>
2022-02-11 18:34:28 +0000

    organização dos exemplos das aulas 4 a 7 do curso em vídeo sobre python

---
+++ aula 8.py
@@ -0,0 +1,12 @@
+#import math
+#num = int(input('Digite um número: '))
+#raiz = math.sqrt(num)
+#print('A raiz de um {} é igual a {}'.format(num, math.ceil(raiz)))
+
+#import random
+#num = random.randint(1, 20)
+#print(num)
+
+import emoji
+
+
