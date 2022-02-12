c8dc73fccea67e65df1f2b9d41ce0fe7a84acbca
Ana Paula <ap.almeidapm@gmail.com>
2022-02-12 13:44:53 +0000

    Anotações da aula 8 sobre python do curso em vídeo, exemplo 16 usando a biblioteca math

---
+++ Exercícios da aula 8/ex016.py
@@ -0,0 +1,9 @@
+import math
+num = float(input('Digite um número: '))
+print('O valor digitado foi {} e sua porção inteira é {}'.format(num, math.trunc(num)))
+
+'''num = float(input('Digite um número: '))
+print('O valo digitado foi {} e a sua porção inteira é {}'.format(num, int(num)))'''
+
+
+
--- aula 8.py
+++ aula 8.py
@@ -1,12 +1,16 @@
-#import math
-#num = int(input('Digite um número: '))
-#raiz = math.sqrt(num)
-#print('A raiz de um {} é igual a {}'.format(num, math.ceil(raiz)))
+import math
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print('A raiz de um {} é igual a {}'.format(num, math.ceil(raiz)))
 
+
+
+#Importando uma biblioteca para numeros aleatorios
 #import random
 #num = random.randint(1, 20)
 #print(num)
 
-import emoji
+
+
 
 
