322a12320e44b181907c3db97706f8256246e3a0
Ana Paula <ap.almeidapm@gmail.com>
2022-02-17 12:13:48 +0000

    organização dos arquivos do curso em vídeo e inclusão do algorítmo usando a biblioteca ramdom para sortear a sequência de nomes de uma apresentação

---
+++ aula 8/ex020.py
@@ -0,0 +1,13 @@
+import random
+aluno1 = str(input('Primeiro aluno: '))
+aluno2 = str(input('Segundo aluno: '))
+aluno3 = str(input('Terceiro aluno: '))
+aluno4 = str(input('Quarto aluno: '))
+
+nome_sorteado = [aluno1, aluno2, aluno3, aluno4]
+random.shuffle(nome_sorteado)
+
+print('A ordem de apresentação será: ')
+print(nome_sorteado)
+
+
