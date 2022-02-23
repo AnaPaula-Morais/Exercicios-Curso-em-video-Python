574f54b994db57f9473edebb58bfe6f030f4c5bf
Ana Paula <ap.almeidapm@gmail.com>
2022-02-23 17:35:34 +0000

    Exemplos utilizando a função upper e a função in

---
+++ aula 9/ex024.py
@@ -0,0 +1,15 @@
+nome_cidade = str(input('Qual o nome da sua cidade: ')).strip()
+#print(nome_cidade.upper().startswith('SANTO'))
+
+print(nome_cidade[:5].upper() == 'SANTO')
+
+
+
+
+
+
+
+
+
+
+
---
+++ aula 9/ex025.py
@@ -0,0 +1,6 @@
+nome = str(input('Digite seu nome: ')).strip()
+print('SILVA' in nome.upper())
+
+#print('Seu nome tem Silva? {}'.format('SILVA' in nome.upper()))
+
+
