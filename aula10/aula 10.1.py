3ac2d6b82980f4e77c80f1c7ed77d6d7717f84dd
Ana Paula <ap.almeidapm@gmail.com>
2022-02-24 19:29:41 +0000

    Exemplos da aula sobre condições no Python

---
+++ aula10/aula 10.1.py
@@ -0,0 +1,8 @@
+nome = str(input('Qual seu nome? '))
+if nome == 'Ana Paula':
+    print('Que nome lindo você tem!!')
+else:
+    print('Seu nome é tão normal!')
+print('Bom dia, {}'.format(nome))
+
+
---
+++ aula10/aula 10.2.py
@@ -0,0 +1,13 @@
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+media = (nota1 + nota2) / 2
+print('A sua média foi {:.1f}'.format(media))
+if media >= 6:
+    print('Parabéns você foi aprovado!!')
+else:
+    print('Infelizmente você não atingiu a média. Tente outra vez!')
+
+
+
+
+
